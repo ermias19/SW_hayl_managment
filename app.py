b502be891a2236887ac74ae35164239b5ae1c380
ermias19 <ermiasmulu19@gmail.com>
2021-07-03 16:34:29 +0300

    second edition final commit

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME']='swhayl19@gmail.com'
 app.config['MAIL_PASSWORD']='swhaylsenbet'
-app.run(threaded=True)
+
 mail=Mail(app)
 
 
