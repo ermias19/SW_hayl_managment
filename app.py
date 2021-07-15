81b08ed920aa4fd68733740a90f59c4048e433c9
ermias19 <ermiasmulu19@gmail.com>
2021-07-15 13:19:38 +0300

    adding student viewer

--- app.py
+++ app.py
@@ -183,10 +183,13 @@ def reset_token(token):
     return render_template('reset_token.html',forms=forms)
 
 
+ 
+
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
+    print(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
